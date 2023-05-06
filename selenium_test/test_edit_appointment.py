e1052137cff71eb08dbc082ea61a07f0ff9f5ece
subhadip1203 <subhadip1203@gmail.com>
2023-05-05 21:06:21 -0700

    all 5 selenium test for Om_hospital

--- selenium_test/test_edit_appointment.py
+++ selenium_test/test_edit_appointment.py
@@ -69,23 +69,6 @@ if test_passed :
         test_passed = False
         print ('test failed')
 
-global_reference = ""
-
-if test_passed:
-    time.sleep(2)
-    patient_reference = driver.find_element(By.XPATH, "//*[@name='name']")
-    patient_name = driver.find_element(By.XPATH, "//*[@name='name']")
-    if patient_reference.get_attribute('innerHTML') and patient_name.get_attribute('innerHTML'):
-        referenceVal = patient_reference.get_attribute('innerHTML') 
-        nameVal = patient_name.get_attribute('innerHTML')
-        global_reference = "["+referenceVal+"] "+nameVal  
-    else:
-        test_passed = False
-        print ('test failed')
-
-print(global_reference)
-
-
 if test_passed:
      # wait for URL to change with 15 seconds timeout
     WebDriverWait(driver, 5).until(EC.url_changes(currentURL))
@@ -93,7 +76,7 @@ if test_passed:
     print(currentURL)
 
 if test_passed :
-    link_elements = driver.find_element(By.XPATH, "//*[@data-menu-xmlid='om_hospital.menu_doctor_root']")
+    link_elements = driver.find_element(By.XPATH, "//*[@data-menu-xmlid='om_hospital.menu_appointment_root']")
     if  link_elements :
         link_elements.click()
         time.sleep(2)
@@ -101,8 +84,9 @@ if test_passed :
         test_passed = False
         print ('test failed')
 
+
 if test_passed :
-    link_elements = driver.find_element(By.XPATH, "//*[@data-menu-xmlid='om_hospital.menu_doctor']")
+    link_elements = driver.find_element(By.XPATH, "//*[@data-menu-xmlid='om_hospital.menu_appointment']")
     if  link_elements :
         print(link_elements.get_attribute('innerHTML'))
         link_elements.click()
@@ -119,34 +103,9 @@ if test_passed:
 
 if test_passed:
     time.sleep(2)
-    cols = driver.find_elements(By.XPATH, "//div[@class='oe_kanban_details']/ul/li/span")
-    if len(cols) > 1 :
-        cols[0].click()
-
-if test_passed:
-     # wait for URL to change with 15 seconds timeout
-    WebDriverWait(driver, 5).until(EC.url_changes(currentURL))
-    currentURL = driver.current_url
-    print(currentURL)
-
-if test_passed:
-    time.sleep(2)
-    span_ele = driver.find_element(By.XPATH, "//span[@class='o_stat_text']")
-    if span_ele and span_ele.get_attribute('innerHTML')== "Appointments" :
-        span_ele.click()
-
-if test_passed:
-     # wait for URL to change with 15 seconds timeout
-    WebDriverWait(driver, 5).until(EC.url_changes(currentURL))
-    currentURL = driver.current_url
-    print(currentURL)
-
-if test_passed :
-    button_elements =driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div/button[3]')
-    if button_elements:
-        if button_elements.get_attribute('innerHTML').strip() == 'Create':
-            button_elements.click()
-            time.sleep(2)
+    appointment_ref = driver.find_element(By.XPATH, "//*[@name='name']")
+    if appointment_ref and appointment_ref.get_attribute('innerHTML') :
+        appointment_ref.click() 
     else:
         test_passed = False
         print ('test failed')
@@ -158,22 +117,15 @@ if test_passed:
     print(currentURL)
 
 if test_passed :
-    time.sleep(2)
-    div1      =   driver.find_element(By.NAME,"patient_id")
-    if div1:
-        div2 = div1.find_element(By.CLASS_NAME,"o_field_many2one_selection")
-        if div2:
-            div3 = div1.find_element(By.CLASS_NAME,"o_input_dropdown")
-            if div3:
-                inputField = div1.find_element(By.TAG_NAME,"input")
-                if inputField :
-                    inputField.click()
-                    time.sleep(5)
-                    dropDown_option     =   driver.find_element(By.XPATH,'//*[@id="ui-id-1"]/li[3]')
-                    if dropDown_option:
-                        dropDown_option.click()
+    button_elements =driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div[1]/div[1]/div/div[1]/button[1]')
+    if button_elements:
+        if button_elements.get_attribute('innerHTML').strip() == 'Edit':
+            button_elements.click()
+            time.sleep(2)
+    else:
+        test_passed = False
+        print ('test failed')
 
-                   
 if test_passed:
     time.sleep(2)
     input_fields    =   driver.find_elements(By.XPATH,'//input[@type="text"]')
@@ -181,12 +133,10 @@ if test_passed:
         for input_field in input_fields:
             if input_field.get_property("name") == 'date_appointment':
                 input_field.clear()
-                input_field.send_keys('05/12/2023')
-
+                input_field.send_keys('05/25/2023')
             if input_field.get_property("name") == 'date_checkup':
                 input_field.clear()
-                input_field.send_keys('05/12/2023 15:30:00')
-
+                input_field.send_keys('05/25/2023 12:30:00')
 
 if test_passed :
     button_elements =driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/button[1]')
@@ -198,10 +148,16 @@ if test_passed :
         test_passed = False
         print ('test failed')
 
-if test_passed:
-     # wait for URL to change with 15 seconds timeout
-    WebDriverWait(driver, 5).until(EC.url_changes(currentURL))
-    currentURL = driver.current_url
-    print(currentURL)
 
-    
+if test_passed:
+    time.sleep(2)
+    appointment_date = driver.find_element(By.NAME, "date_appointment")
+    appointment_dateCheckup = driver.find_element(By.NAME, "date_checkup")
+    if appointment_date and appointment_dateCheckup:
+        if appointment_date.get_attribute('innerHTML') == '05/25/2023' and appointment_dateCheckup.get_attribute('innerHTML') == '05/25/2023 12:30:00':
+            print("Final : test successful")
+        else:
+            print("Final : test failed")
+    else:
+        print("test failed")
+
